80f34d5bbe60b36aa89780997fd0d504c9bb6645
Динара Алдарова <dinara_aldarova@mail.ru>
2019-04-26 14:45:29 +0500

    Код дописан и работает

---
+++ Fermat's factorization attack/Culc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace Fermat_s_factorization_attack
+{
+public class Culculate
+{
+    public string report;
+    public Culculate()
+    {
+        report = "";
+    }
+    public void NewFact(string str_N)
+    {
+        BigInteger N = BigInteger.Parse(str_N);
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        BigInteger k = Sqrt(N) + 1;
+        BigInteger y = k * k - N;
+        BigInteger d = 1;
+
+        while (Sqrt(y) * Sqrt(y) != y && Sqrt(y) < N / 2)
+        {
+            y = y + 2 * k + d;
+            d += 2;
+        }
+        stopwatch.Stop();
+        if (Sqrt(y) >= N / 2)
+        {
+            report += "Множители не найдены\n";
+        }
+        if (Sqrt(y) * Sqrt(y) == y)
+        {
+            BigInteger x = Sqrt(N + y);
+            y = Sqrt(y);
+            report += "Нетривиальные делители " + N.ToString() + ": " + (x + y).ToString() + " и " + (x - y).ToString() + ", разбег " + (y * 2).ToString() + "\n";
+        }
+
+        report += "Время работы алгоритма " + stopwatch.Elapsed.TotalMilliseconds + " мс\n\n";
+        stopwatch.Reset();
+    }
+
+    public BigInteger Sqrt(BigInteger n)
+    {
+        if (n == 0) return 0;
+        if (n > 0)
+        {
+            int bitLength = Convert.ToInt32(Math.Ceiling(BigInteger.Log(n, 2)));
+            BigInteger root = BigInteger.One << (bitLength / 2);
+
+            while (!isSqrt(n, root))
+            {
+                root += n / root;
+                root /= 2;
+            }
+
+            return root;
+        }
+
+        throw new ArithmeticException("NaN");
+    }
+
+    private Boolean isSqrt(BigInteger n, BigInteger root)
+    {
+        BigInteger lowerBound = root * root;
+        BigInteger upperBound = (root + 1) * (root + 1);
+
+        return (n >= lowerBound && n < upperBound);
+    }
+}
+}
--- Fermat's factorization attack/Form1.cs
+++ Fermat's factorization attack/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Numerics;
 
 namespace Fermat_s_factorization_attack
 {
     public partial class Form1 : Form
     {
+        Culculate culc;
         public Form1()
         {
             InitializeComponent();
+            culc = new Culculate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            culc.NewFact(textBox1.Text);
+            richTextBox1.Text = culc.report;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!(Char.IsDigit(number) || number == 8)) // only digit or backspace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BigInteger m1 = BigInteger.Parse(textBox2.Text);
+            BigInteger m2 = BigInteger.Parse(textBox3.Text);
+            BigInteger N = BigInteger.Multiply(m1, m2);
+            textBox1.Text = N.ToString();
         }
     }
 }
